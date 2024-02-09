bac3be545dacf52e4e116cc3a2099c31f95d734b
4nth0ny1 <acatullo4@gmail.com>
2024-02-09 16:31:29 -0600

    winmain added

---
+++ Main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#pragma warning(push, 0)
+#include <windows.h>
+#pragma warning(pop)
+
+int WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance,
+    PSTR CommandLine, int CmdShow)
+{
+    UNREFERENCED_PARAMETER(Instance);
+    UNREFERENCED_PARAMETER(PreviousInstance);
+    UNREFERENCED_PARAMETER(CommandLine);
+    UNREFERENCED_PARAMETER(CmdShow);
+
+    return 0;
+}
