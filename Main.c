924a4dd1e092bb501a9b2ab16808bcb51ad291cd
4nth0ny1 <acatullo4@gmail.com>
2024-02-09 18:31:57 -0600

    removed old comments

--- Main.c
+++ Main.c
@@ -7,20 +7,16 @@
 #include "Main.h"
 
 
-
-// 1. Main 
 int WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, PSTR CommandLine, int CmdShow)
 {
     UNREFERENCED_PARAMETER(PreviousInstance);
     UNREFERENCED_PARAMETER(CommandLine);
     UNREFERENCED_PARAMETER(CmdShow);
 
-    // 18. call Create MainWindowFunction
     if (CreateMainGameWindow(Instance) != ERROR_SUCCESS) {
         goto Exit;
     }
 
-    // 3. Message Loop 
     MSG Message = { 0 };
 
     while (GetMessageA(&Message, NULL, 0, 0) > 0) {
@@ -33,16 +29,13 @@ Exit:
 }
 
 
-// 2. Window
-// 10. cleaned up this function, renamed parameters, created result, made everything default response
-// 19. Added SAL - Source annotation lang
+
 LRESULT CALLBACK MainWindowProc(_In_ HWND WindowHandle, _In_ UINT Message, _In_ WPARAM WParam, _In_ LPARAM LParam)
 {
     LRESULT Result = 0;
 
     switch (Message)
     {
-        // 22. adding ability to shutdown program on exit.
         case WM_CLOSE: {
             PostQuitMessage(0);
 
@@ -55,16 +48,10 @@ LRESULT CALLBACK MainWindowProc(_In_ HWND WindowHandle, _In_ UINT Message, _In_ 
     return Result;
 }
 
-// 13. DWORD CreateMainGameWindow() 
-// 19. Added SAL - Source annotation lang
-// 20. removed instance parameter and added void
+
 DWORD CreateMainGameWindow(void) {
     DWORD Result = ERROR_SUCCESS;
 
-    // 14. copy and paste all window stuff from the winmain function and moved it to this function
-    
-    // 4. Register a window class
-    // 11. GetModuleHandleA
     WNDCLASSEXA WindowClass = { 0 };
     HWND WindowHandle = 0;
 
@@ -82,17 +69,13 @@ DWORD CreateMainGameWindow(void) {
     WindowClass.lpszClassName = "Game_B_ALT_WindowClass";
     WindowClass.hIconSm = LoadIconA(NULL, IDI_APPLICATION);
 
-    // 5. Register class comes from winprog website, but docs is #5 in bookmarks
     if (!RegisterClassExA(&WindowClass))
     {
-        // 16. 
         Result = GetLastError();
         MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
         goto Exit;
     }
 
-    // 5. Create the window
-    // 21. Replaced instance with WindowClass.hInstance which gets from GetModule ...
     WindowHandle = CreateWindowExA(
         WS_EX_CLIENTEDGE,
         WindowClass.lpszClassName,
@@ -103,14 +86,12 @@ DWORD CreateMainGameWindow(void) {
 
     if (WindowHandle == NULL)
     {
-        // 17.
         Result = GetLastError();
         MessageBox(NULL, "Window Creation Failed!", "Error!",
             MB_ICONEXCLAMATION | MB_OK);
         goto Exit;
     }
 
-    // 15. Exit Message 
 Exit:
     return 0;
 }
