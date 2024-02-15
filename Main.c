2fd6c2f65d57c9b24332aa3b27ef34e94a2049ac
4nth0ny1 <acatullo4@gmail.com>
2024-02-14 19:30:39 -0600

    removed whitespace

--- Main.c
+++ Main.c
@@ -1,51 +1,35 @@
 #include <stdio.h>
-
 #pragma warning(push, 3)
-
 #include <windows.h>
-
 #pragma warning(pop)
 
 #include <stdint.h>
-
 #include "Main.h"
 
 HWND gGameWindow;
-
 BOOL gGameIsRunning;
-
 GAMEBITMAP gBackBuffer;
-
 GAMEPERFDATA gPerformanceData;
 
 
 int __stdcall WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, PSTR CommandLine, INT CmdShow)
 {
     UNREFERENCED_PARAMETER(Instance);
-
     UNREFERENCED_PARAMETER(PreviousInstance);
-
     UNREFERENCED_PARAMETER(CommandLine);
-
     UNREFERENCED_PARAMETER(CmdShow);
 
     MSG Message = { 0 };
-
     int64_t FrameStart = 0;
-
     int64_t FrameEnd = 0;
-
     int64_t ElapsedMicrosecondsPerFrame;
-
     int64_t ElapsedMicrosecondsPerFrameAccumulatorRaw = 0;
-
     int64_t ElapsedMicrosecondsPerFrameAccumulatorCooked = 0;
 
 
     if (GameIsAlreadyRunning() == TRUE)
     {
         MessageBoxA(NULL, "Another instance of this program is already running!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-
         goto Exit;
     }
 
@@ -54,35 +38,23 @@ int __stdcall WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, PSTR Comma
         goto Exit;
     }
 
-
     QueryPerformanceFrequency((LARGE_INTEGER*)&gPerformanceData.PerfFrequency);
 
-
-
     gBackBuffer.BitmapInfo.bmiHeader.biSize = sizeof(gBackBuffer.BitmapInfo.bmiHeader);
-
     gBackBuffer.BitmapInfo.bmiHeader.biWidth = GAME_RES_WIDTH;
-
     gBackBuffer.BitmapInfo.bmiHeader.biHeight = GAME_RES_HEIGHT;
-
     gBackBuffer.BitmapInfo.bmiHeader.biBitCount = GAME_BPP;
-
     gBackBuffer.BitmapInfo.bmiHeader.biCompression = BI_RGB;
-
     gBackBuffer.BitmapInfo.bmiHeader.biPlanes = 1;
-
     gBackBuffer.Memory = VirtualAlloc(NULL, GAME_DRAWING_AREA_MEMORY_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 
     if (gBackBuffer.Memory == NULL)
     {
         MessageBoxA(NULL, "Failed to allocate memory for drawing surface!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-
         goto Exit;
     }
 
     memset(gBackBuffer.Memory, 0x7F, GAME_DRAWING_AREA_MEMORY_SIZE);
-
-
 
     gGameIsRunning = TRUE;
 
@@ -96,15 +68,11 @@ int __stdcall WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, PSTR Comma
         }
 
         ProcessPlayerInput();
-
         RenderFrameGraphics();
-
         QueryPerformanceCounter((LARGE_INTEGER*)&FrameEnd);
 
         ElapsedMicrosecondsPerFrame = FrameEnd - FrameStart;
-
         ElapsedMicrosecondsPerFrame *= 1000000;
-
         ElapsedMicrosecondsPerFrame /= gPerformanceData.PerfFrequency;
 
         gPerformanceData.TotalFramesRendered++;
@@ -114,30 +82,20 @@ int __stdcall WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, PSTR Comma
         while (ElapsedMicrosecondsPerFrame <= TARGET_MICROSECONDS_PER_FRAME)
         {
             Sleep(0);
-
             ElapsedMicrosecondsPerFrame = FrameEnd - FrameStart;
-
             ElapsedMicrosecondsPerFrame *= 1000000;
-
             ElapsedMicrosecondsPerFrame /= gPerformanceData.PerfFrequency;
-
             QueryPerformanceCounter((LARGE_INTEGER*)&FrameEnd);
         }
 
         ElapsedMicrosecondsPerFrameAccumulatorCooked += ElapsedMicrosecondsPerFrame;
 
-
-
         if ((gPerformanceData.TotalFramesRendered % CALCULATE_AVG_FPS_EVERY_X_FRAMES) == 0)
         {
             int64_t AverageMicrosecondsPerFrameRaw = ElapsedMicrosecondsPerFrameAccumulatorRaw / CALCULATE_AVG_FPS_EVERY_X_FRAMES;
-
             int64_t AverageMicrosecondsPerFrameCooked = ElapsedMicrosecondsPerFrameAccumulatorCooked / CALCULATE_AVG_FPS_EVERY_X_FRAMES;
-
             gPerformanceData.RawFPSAverage = 1.0f / ((ElapsedMicrosecondsPerFrameAccumulatorRaw / 60) * 0.000001f);
-
             gPerformanceData.CookedFPSAverage = 1.0f / ((ElapsedMicrosecondsPerFrameAccumulatorCooked / 60) * 0.000001f);
-
 
             char str[256] = { 0 };
 
@@ -150,15 +108,12 @@ int __stdcall WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, PSTR Comma
             OutputDebugStringA(str);
 
             ElapsedMicrosecondsPerFrameAccumulatorRaw = 0;
-
             ElapsedMicrosecondsPerFrameAccumulatorCooked = 0;
         }
     }
 
 
 Exit:
-
-
     return(0);
 }
 
@@ -168,18 +123,16 @@ LRESULT CALLBACK MainWindowProc(_In_ HWND WindowHandle, _In_ UINT Message, _In_ 
 
     switch (Message)
     {
-    case WM_CLOSE:
-    {
-        gGameIsRunning = FALSE;
-
-        PostQuitMessage(0);
-
-        break;
-    }
-    default:
-    {
-        Result = DefWindowProcA(WindowHandle, Message, WParam, LParam);
-    }
+        case WM_CLOSE:
+        {
+            gGameIsRunning = FALSE;
+            PostQuitMessage(0);
+            break;
+        }
+        default:
+        {
+            Result = DefWindowProcA(WindowHandle, Message, WParam, LParam);
+        }
     }
 
     return(Result);
@@ -188,31 +141,19 @@ LRESULT CALLBACK MainWindowProc(_In_ HWND WindowHandle, _In_ UINT Message, _In_ 
 DWORD CreateMainGameWindow(void)
 {
     DWORD Result = ERROR_SUCCESS;
-
     WNDCLASSEXA WindowClass = { 0 };
 
     WindowClass.cbSize = sizeof(WNDCLASSEXA);
-
     WindowClass.style = 0;
-
     WindowClass.lpfnWndProc = MainWindowProc;
-
     WindowClass.cbClsExtra = 0;
-
     WindowClass.cbWndExtra = 0;
-
     WindowClass.hInstance = GetModuleHandleA(NULL);
-
     WindowClass.hIcon = LoadIconA(NULL, IDI_APPLICATION);
-
     WindowClass.hIconSm = LoadIconA(NULL, IDI_APPLICATION);
-
     WindowClass.hCursor = LoadCursorA(NULL, IDC_ARROW);
-
     WindowClass.hbrBackground = CreateSolidBrush(RGB(255, 0, 255));
-
     WindowClass.lpszMenuName = NULL;
-
     WindowClass.lpszClassName = GAME_NAME "_WINDOWCLASS";
 
     // SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
@@ -220,9 +161,7 @@ DWORD CreateMainGameWindow(void)
     if (RegisterClassExA(&WindowClass) == 0)
     {
         Result = GetLastError();
-
         MessageBoxA(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-
         goto Exit;
     }
 
@@ -231,9 +170,7 @@ DWORD CreateMainGameWindow(void)
     if (gGameWindow == NULL)
     {
         Result = GetLastError();
-
         MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-
         goto Exit;
     }
 
@@ -242,18 +179,15 @@ DWORD CreateMainGameWindow(void)
     if (GetMonitorInfoA(MonitorFromWindow(gGameWindow, MONITOR_DEFAULTTOPRIMARY), &gPerformanceData.MonitorInfo) == 0)
     {
         Result = ERROR_MONITOR_NO_DESCRIPTOR;
-
         goto Exit;
     }
 
     gPerformanceData.MonitorWidth = gPerformanceData.MonitorInfo.rcMonitor.right - gPerformanceData.MonitorInfo.rcMonitor.left;
-
     gPerformanceData.MonitorHeight = gPerformanceData.MonitorInfo.rcMonitor.bottom - gPerformanceData.MonitorInfo.rcMonitor.top;
 
     if (SetWindowLongPtrA(gGameWindow, GWL_STYLE, (WS_OVERLAPPEDWINDOW | WS_VISIBLE) & ~WS_OVERLAPPEDWINDOW) == 0)
     {
         Result = GetLastError();
-
         goto Exit;
     }
 
@@ -266,20 +200,17 @@ DWORD CreateMainGameWindow(void)
         SWP_NOOWNERZORDER | SWP_FRAMECHANGED) == 0)
     {
         Result = GetLastError();
-
         goto Exit;
     }
 
 
 Exit:
-
     return(Result);
 }
 
 BOOL GameIsAlreadyRunning(void)
 {
     HANDLE Mutex = NULL;
-
     Mutex = CreateMutexA(NULL, FALSE, GAME_NAME "_GameMutex");
 
     if (GetLastError() == ERROR_ALREADY_EXISTS)
@@ -305,13 +236,9 @@ void ProcessPlayerInput(void)
 void RenderFrameGraphics(void)
 {
     PIXEL32 Pixel = { 0 };
-
     Pixel.Blue = 0x7f;
-
     Pixel.Green = 0;
-
     Pixel.Red = 0;
-
     Pixel.Alpha = 0xff;
 
     for (int x = 0; x < GAME_RES_WIDTH * GAME_RES_HEIGHT; x++)
